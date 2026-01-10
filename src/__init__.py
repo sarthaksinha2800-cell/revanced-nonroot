49aca0442b3f6ed0b79cb183b2ebaa614c2587d7
sarthaksinha2800-cell <sarthaksinha2800@gmail.com>
2026-01-10 19:32:31 +0530

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -64,12 +64,13 @@ session.headers.update({
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
     'Accept-Language': 'en-US,en;q=0.5',
     'Accept-Encoding': 'gzip, deflate, br',
+    'Referer': 'https://www.google.com/', # Makes it look like you came from a search engine
     'DNT': '1',
     'Connection': 'keep-alive',
     'Upgrade-Insecure-Requests': '1',
     'Sec-Fetch-Dest': 'document',
     'Sec-Fetch-Mode': 'navigate',
-    'Sec-Fetch-Site': 'none',
+    'Sec-Fetch-Site': 'cross-site',
     'Sec-Fetch-User': '?1',
 })
 
