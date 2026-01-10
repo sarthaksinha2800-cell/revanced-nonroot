3f91baf2b1a1482d4eef182c288257f18892dd21
sarthaksinha2800-cell <sarthaksinha2800@gmail.com>
2026-01-10 19:31:13 +0530

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -64,13 +64,13 @@ session.headers.update({
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
     'Accept-Language': 'en-US,en;q=0.5',
     'Accept-Encoding': 'gzip, deflate, br',
+    'DNT': '1',
     'Connection': 'keep-alive',
     'Upgrade-Insecure-Requests': '1',
     'Sec-Fetch-Dest': 'document',
     'Sec-Fetch-Mode': 'navigate',
     'Sec-Fetch-Site': 'none',
     'Sec-Fetch-User': '?1',
-    'Cache-Control': 'max-age=0',
 })
 
 # Logging
